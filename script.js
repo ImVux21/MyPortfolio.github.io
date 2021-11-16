b26b7ce14752cdd0c30126d7f364e119a30035f6
ImVux21 <88678046+ImVux21@users.noreply.github.com>
2021-11-16 09:37:34 +0700

    My Portfolio

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ $(window).on('load', function(event) {
     $('body').removeClass('preloading');
     $('.loader').delay(1500).fadeOut('fast');
 });
+
+var btnContainer = document.getElementsByClassName("menu");
+var btns = btnContainer.getElementsByClassName("menu-link");
+
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener('click', function() {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active");
+        this.className += " active";
+    })
+}
